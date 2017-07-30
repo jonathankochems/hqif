7a710cba40b49253bbd7c0767a454acaddf96924
Jonathan Kochems <jonathan.kochems@gmail.com>
2017-07-30 15:30:41 +0100

    Fix warnings in tests.

--- test/HQifSpec.hs
+++ test/HQifSpec.hs
@@ -24,26 +24,27 @@ import Test.Hspec
 import Test.QuickCheck
 import Test.QuickCheck.Gen(oneof)
 
+spec :: SpecWith ()
 spec = 
   describe "Data.Qif" $ do
     it "should satisfy that displaying and parsing yields the identity on one transaction" $ do
-      let qif = Qif {accountType = Bank, transactions = [Transaction {_date = Date.fromGregorian 2018 4 3, _payee = "r\227\245\&5\191!", _memo = ";\226S\224:+>aC1ik", _amount = -1037.0022, _category = Just "D:RlR", _investmentAction = Nothing, _clearedStatus = NotCleared, _splits = []}]}
-          _parsedQif = qifFromString $ displayQif qif
-      displayQif _parsedQif `shouldBe` displayQif qif
-      _parsedQif `shouldBe` qif
+      let _qif = Qif {accountType = Bank, transactions = [Transaction {_date = Date.fromGregorian 2018 4 3, _payee = "r\227\245\&5\191!", _memo = ";\226S\224:+>aC1ik", _amount = -1037.0022, _category = Just "D:RlR", _investmentAction = Nothing, _clearedStatus = NotCleared, _splits = []}]}
+          _parsedQif = qifFromString $ displayQif _qif
+      displayQif _parsedQif `shouldBe` displayQif _qif
+      _parsedQif `shouldBe` _qif
     it "should satisfy that displaying and parsing yields the identity on serveral transactions" $ do
-      let qif = Qif { accountType = Invoice
-                    , transactions = [Transaction {_date = Date.fromGregorian 2013 9 24, _payee = "\253y\247K\201\201", _memo = "\225+pA\239\&1k[", _amount = -3760.5286, _category = Just "\183j\253O`_yZ\245Xz+8\231e", _investmentAction = Nothing, _clearedStatus = Cleared, _splits = []}
-                                     ,Transaction {_date = Date.fromGregorian 2014 8 17, _payee = "\198\\trrw}", _memo = "4\223},Z\243\&1]o=", _amount = -262.1128, _category = Nothing, _investmentAction = Just "g|6", _clearedStatus = Reconciled, _splits = []}
-                                     ,Transaction {_date = Date.fromGregorian 2016 6 21, _payee = "W8", _memo = "'O15$\172W\234.KC>h\172\207|S", _amount = 2330.6196, _category = Just "xM1X\228\206", _investmentAction = Nothing, _clearedStatus = Reconciled, _splits = []}
-                                     ,Transaction {_date = Date.fromGregorian 2017 4 30, _payee = "_dRdm\213C\"\175dnT@ Kz\210", _memo = "p\197AN%\"tP\233)eh9o^\233;", _amount = -3659.8916, _category = Nothing, _investmentAction = Nothing, _clearedStatus = NotCleared, _splits = []}
-                                     ]
-                    }
-          _parsedQif = qifFromString $ displayQif qif
-      displayQif _parsedQif `shouldBe` displayQif qif
-      _parsedQif `shouldBe` qif
+      let _qif = Qif { accountType = Invoice
+                     , transactions = [Transaction {_date = Date.fromGregorian 2013 9 24, _payee = "\253y\247K\201\201", _memo = "\225+pA\239\&1k[", _amount = -3760.5286, _category = Just "\183j\253O`_yZ\245Xz+8\231e", _investmentAction = Nothing, _clearedStatus = Cleared, _splits = []}
+                                      ,Transaction {_date = Date.fromGregorian 2014 8 17, _payee = "\198\\trrw}", _memo = "4\223},Z\243\&1]o=", _amount = -262.1128, _category = Nothing, _investmentAction = Just "g|6", _clearedStatus = Reconciled, _splits = []}
+                                      ,Transaction {_date = Date.fromGregorian 2016 6 21, _payee = "W8", _memo = "'O15$\172W\234.KC>h\172\207|S", _amount = 2330.6196, _category = Just "xM1X\228\206", _investmentAction = Nothing, _clearedStatus = Reconciled, _splits = []}
+                                      ,Transaction {_date = Date.fromGregorian 2017 4 30, _payee = "_dRdm\213C\"\175dnT@ Kz\210", _memo = "p\197AN%\"tP\233)eh9o^\233;", _amount = -3659.8916, _category = Nothing, _investmentAction = Nothing, _clearedStatus = NotCleared, _splits = []}
+                                      ]
+                     }
+          _parsedQif = qifFromString $ displayQif _qif
+      displayQif _parsedQif `shouldBe` displayQif _qif
+      _parsedQif `shouldBe` _qif
     it "should be able to take a qif, print it as a string, and parse it and result in the original qif" $
-      property $ forAll qif $ \qif -> qifFromString (displayQif qif) == qif
+      property $ forAll qif $ \_qif -> qifFromString (displayQif _qif) == _qif
 
 
 dateGenerator :: Gen Date.Day 
@@ -60,23 +61,23 @@ transaction = do
    iv  <- choose(0,5)
    c'  <- choose(False,True)
    iv' <- choose(False,True)
-   s'  <- choose(False,True)
    description <- suchThat (vector j) isText
    text        <- suchThat (vector k) isText
    balance     <- choose(-5000.0,5000.0)
-   category    <- maybeGen c'  <$> suchThat (vector c) isText
+   _category   <- maybeGen c'  <$> suchThat (vector c) isText
    inv_act     <- maybeGen iv' <$> suchThat (vector iv) isText
    clr_sta     <- oneof $ map return [NotCleared, Cleared, Reconciled] 
-   date        <- dateGenerator
-   return Transaction{ _date = date, _payee = description, _memo = text, _amount = balance
-                     , _category = category, _investmentAction = inv_act, _clearedStatus = clr_sta
+   _date       <- dateGenerator
+   return Transaction{ _date = _date, _payee = description, _memo = text, _amount = balance
+                     , _category = _category, _investmentAction = inv_act, _clearedStatus = clr_sta
                      , _splits = [] }
   where maybeGen False _  = Nothing
         maybeGen True  x  = Just x
 
 qif :: Gen Qif
 qif = do typeinfo  <- oneof $ map return [ Cash, Bank, CCard, Invst, OthA, OthL, Invoice ]
-         transactions <- listOf transaction
-         return Qif{ accountType = typeinfo, transactions = transactions }
+         _transactions <- listOf transaction
+         return Qif{ accountType = typeinfo, transactions = _transactions }
 
+isText :: String -> Bool
 isText = all isPrint
