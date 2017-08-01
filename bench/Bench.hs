90818dbf349c614387119b3bf42bde01e5215796
Jonathan Kochems <jonathan.kochems@gmail.com>
2017-08-01 20:59:12 +0100

    merging benchmarks

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -9,14 +9,24 @@
 --
 -- Description
 --
+{-# LANGUAGE FlexibleContexts, BangPatterns #-}
 module Bench where
+
+-- Control
+import Control.Monad(forM)
+
+-- Text
+import qualified Text.Parsec as Parsec
 
 -- Qif
 import Data.Qif
+import Data.Qif.Sample(partialString, months, typeParser, transactionTypes)
 
 -- Data
 import Data.Char(isPrint)
 import qualified Data.Time.Calendar as Date
+import Data.Maybe(fromMaybe)
+import Data.Functor.Identity(Identity)
 
 -- Criterion
 import Criterion.Main
@@ -26,8 +36,13 @@ import Test.QuickCheck
 
 main :: IO ()
 main = defaultMain [
-              bench "generateSerialiseDeserialseBench" $ nfIO generateSerialiseDeserialseBench
-         ]
+                 bench "generateSerialiseDeserialseBench" $ nfIO generateSerialiseDeserialseBench
+               , bench "partial_string" $ nfIO (partialStringBench n)
+               , bench "string"         $ nfIO (stringBench n)
+               , bench "partial_types"  $ nfIO (partialTypesBench n)
+               , bench "base"           $ nfIO (baseBench n)
+            ]
+  where n     =  1000
 
 generateSerialiseDeserialseBench :: IO ()
 generateSerialiseDeserialseBench = 
@@ -66,3 +81,44 @@ qif = do typeinfo  <- oneof $ map return [ Cash, Bank, CCard, Invst, OthA, OthL,
 
 isText :: String -> Bool
 isText = all isPrint
+
+assert :: Monad m => Bool -> m ()
+assert True  = return ()
+assert False = error "assertion failure"
+
+parse :: Parsec.Stream s Identity t => Parsec.Parsec s () a -> s -> Either Parsec.ParseError a
+parse p = Parsec.parse p "(unknown)"
+
+partialStringBench :: Int -> IO ()
+partialStringBench n = do rs <- forM (zip [1..n] $ cycle months) $ \(_,m) -> do
+                                  (!_rs) <- forM months $ \m' -> do 
+                                              let result = either (const False) ((==) m') $ parse (partialString m) m' 
+                                              return result
+                                  return $ or _rs
+                          assert $ and rs
+                          return ()
+
+partialTypesBench :: Int -> IO ()
+partialTypesBench n = do rs <- forM (zip [1..n] $ cycle transactionTypes) $ \(_,t) -> do
+                                 let result = either (const False) ((==) t) $ fromMaybe "" <$> parse (typeParser) t 
+                                 return result
+                         assert $ and rs
+                         return ()
+
+stringBench :: Int -> IO ()
+stringBench n = do rs <- forM (zip [1..n] $ cycle months) $ \(_,m) -> do
+                           (!_rs) <- forM months $ \m' -> do 
+                                       let result = either (const False) ((==) m') $ parse (Parsec.string m) m' 
+                                       return result
+                           return $ or _rs
+                   assert $ and rs
+                   return ()
+
+baseBench :: Int -> IO ()
+baseBench n = do rs <- forM (zip [1..n] $ cycle months) $ \(_,m) -> do
+                         (!_rs) <- forM months $ \m' -> do 
+                                     let result = either (const False) ((==) m') $ const (Right m) m' 
+                                     return result
+                         return $ or _rs
+                 assert $ and rs
+                 return ()
